4ae96ce1077e23c72828b80ad6945d488957265d
Bernez <manuel.bernez@gmail.com>
2018-09-04 23:58:33 -0500

    Se agregó pagina cursos con un poco de diseño y sus recursos

---
+++ Talento/Controllers/TalentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Talento.Controllers
+{
+    [Route("CoreUI")]
+    public class TalentController : Controller
+    {
+        [Route("{view=Index}")]
+        public IActionResult Index(string view)
+        {
+            ViewData["Title"] = "Páginas de Talent";
+            return View(view);
+        }
+    }
+}
+
